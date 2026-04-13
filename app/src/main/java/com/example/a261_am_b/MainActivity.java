ff853709b73d31c33524b86528ec4cac8aba162a
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2026-04-13 19:40:53 -0300

    feat: add click event on new Button

--- app/src/main/java/com/example/a261_am_b/MainActivity.java
+++ app/src/main/java/com/example/a261_am_b/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.a261_am_b;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +22,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Button viewImage = new Button(this);
+        viewImage.setText("Mostrar imagen");
+        viewImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("main-log", "viewImage.onClick fue ejecutado");
+            }
+        });
+
+        LinearLayout loginContainer = findViewById(R.id.login_container);
+        loginContainer.addView(viewImage);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);

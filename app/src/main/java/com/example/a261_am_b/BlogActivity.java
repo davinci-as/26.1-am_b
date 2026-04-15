242e8330505f551f64efa79393a1095616d5c85d
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2026-04-15 20:33:56 -0300

    feat: add blog activity

--- app/src/main/java/com/example/a261_am_b/BlogActivity.java
+++ app/src/main/java/com/example/a261_am_b/BlogActivity.java
@@ -3,8 +3,6 @@ package com.example.a261_am_b;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -13,26 +11,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class BlogActivity extends AppCompatActivity {
+    public void sendMessage(View view) {
+        Log.i("main-log", "sendMessage fue ejecutado");
+        TextView loginMessage = findViewById(R.id.blog_message);
+        loginMessage.setText(R.string.login_text_alternative);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        Button viewImage = new Button(this);
-        viewImage.setText(R.string.login_cta_text);
-        viewImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i("main-log", "viewImage.onClick fue ejecutado");
-            }
-        });
-
-        LinearLayout loginContainer = findViewById(R.id.login_container);
-        loginContainer.addView(viewImage);
-
+        setContentView(R.layout.activity_blog);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);

616355bd0857430995190790abbe69a9f7c61811
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2026-06-03 20:05:21 -0300

    feat: create user using firebase auth

--- app/src/main/java/com/example/a261_am_b/SignupActivity.java
+++ app/src/main/java/com/example/a261_am_b/SignupActivity.java
@@ -1,6 +1,10 @@
 package com.example.a261_am_b;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +12,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SignupActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+
+        if(mAuth.getCurrentUser() != null) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_signup);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -21,4 +39,58 @@ public class SignupActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    private boolean checkEmail (String email) {
+        //check por regex
+        //verificar manualmente
+        return true;
+    }
+
+    private boolean checkPassword (String email) {
+        //check por regex
+        //verificar manualmente
+        return true;
+    }
+
+    private boolean samePassword (String email, String emailVerification) {
+        return true;
+    }
+
+    public void createUser(View v) {
+        String signupEmail = ((TextView) findViewById(R.id.signupEmail)).getText().toString();
+        String signupPassword = ((TextView) findViewById(R.id.signupPassword)).getText().toString();
+        String signupPasswordVerification = ((TextView) findViewById(R.id.signupPasswordVerification)).getText().toString();
+
+        if(!checkEmail(signupEmail)) {
+            //Modal con mensaje
+            return;
+        };
+
+        if(!checkPassword(signupEmail)) {
+            //Modal con mensaje
+            return;
+        };
+
+        if(!checkPassword(signupPasswordVerification)) {
+            //Modal con mensaje
+            return;
+        };
+
+        if(!samePassword(signupPassword, signupPasswordVerification)) {
+            //Modal con mensaje
+            return;
+        };
+
+
+        mAuth.createUserWithEmailAndPassword(signupEmail, signupPassword).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                FirebaseUser user = authResult.getUser();
+                if(user == null) return;
+                Log.i("create-user", user.getUid());
+                //user.sendEmailVerification();
+            }
+        });
+    }
+
 }

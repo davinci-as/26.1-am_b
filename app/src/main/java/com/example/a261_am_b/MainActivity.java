d50c61afddd42038dedc3eafc3a64b252a5d372d
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2026-06-03 20:31:48 -0300

    chore: add firebase firestore

--- app/src/main/java/com/example/a261_am_b/MainActivity.java
+++ app/src/main/java/com/example/a261_am_b/MainActivity.java
@@ -22,10 +22,15 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
     public static final int ACTIVITY_OPTION = 1;
     private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -45,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
-
+        db = FirebaseFirestore.getInstance();
 
         FirebaseUser currentUser = mAuth.getCurrentUser();
 
